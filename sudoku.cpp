e97c3780d8e218d94439d7a7eb687c119549fa1d
Harsh Bilodiya <harshb@bgsu.edu>
2016-04-19 10:19:03 -0400

    Modified CPP

--- sudoku.cpp
+++ sudoku.cpp
@@ -18,6 +18,7 @@ using namespace std;
 #define N 9
 
 // Headers for all functions (BGSU style)
+void printMessage();
 bool FindEmptyLocation(int grid[N][N], int &row, int &col);
 bool isSafe(int grid[N][N], int row, int col, int num);
 bool solveSudoku(int grid[N][N]);
@@ -27,6 +28,7 @@ bool UsedInBox(int grid[N][N], int boxStartRow, int boxStartCol, int num);
 void printGrid(int grid[N][N]);
 
 int main() {
+	// Have user enter thier paritially filled puzzle 
 	int grid[N][N] = {{3, 0, 6, 5, 0, 8, 4, 0, 0},
                       {5, 2, 0, 0, 0, 0, 0, 0, 0},
                       {0, 8, 7, 0, 0, 0, 0, 3, 1},
@@ -36,9 +38,11 @@ int main() {
                       {1, 3, 0, 0, 0, 0, 2, 5, 0},
                       {0, 0, 0, 0, 0, 0, 0, 7, 4},
                       {0, 0, 5, 2, 0, 6, 3, 0, 0}};
+	
+	// Print welcome message
+	printMessage();
                       
     if (solveSudoku(grid) == true) {
-    	cout << "\nHello, Welcome to this sudoku solver" << endl;
     	cout << "The solution to the sudoku defined in the program is: " << endl << endl;
     	// Use printGrid function to print solution
     	printGrid(grid);
@@ -51,9 +55,19 @@ int main() {
 
 // Implement functions
 
+// Print a message when program is run
+void printMessage() {
+	cout << endl;
+	cout << "*****************************************" << endl;
+        cout << "*****************************************" << endl;
+	cout << "**                HELLO                **" << endl;
+	cout << "**    WELCOME TO OUR SUDOKU SOLVER     **" << endl;
+        cout << "*****************************************" << endl;
+        cout << "*****************************************" << endl;
+}                
+
 // Assign values to all unassigned locations for Sudoku solution
-bool solveSudoku(int grid[N][N])
-{
+bool solveSudoku(int grid[N][N]) {
     int row, col;
     if (!FindEmptyLocation(grid, row, col))
        return true;
@@ -71,8 +85,7 @@ bool solveSudoku(int grid[N][N])
 }
 
 // Searches the grid to find an entry that is still unassigned
-bool FindEmptyLocation(int grid[N][N], int &row, int &col)
-{
+bool FindEmptyLocation(int grid[N][N], int &row, int &col) {
     for (row = 0; row < N; row++)
         for (col = 0; col < N; col++)
             if (grid[row][col] == EMPTY)
@@ -81,8 +94,7 @@ bool FindEmptyLocation(int grid[N][N], int &row, int &col)
 }
 
 // Returns whether any assigned entry n the specified row matches given number
-bool UsedInRow(int grid[N][N], int row, int num)
-{
+bool UsedInRow(int grid[N][N], int row, int num) {
     for (int col = 0; col < N; col++)
         if (grid[row][col] == num)
             return true;
@@ -90,8 +102,7 @@ bool UsedInRow(int grid[N][N], int row, int num)
 }
 
 // Returns whether any assigned entry in the specified column matches given number
-bool UsedInCol(int grid[N][N], int col, int num)
-{
+bool UsedInCol(int grid[N][N], int col, int num) {
     for (int row = 0; row < N; row++)
         if (grid[row][col] == num)
             return true;
@@ -99,8 +110,7 @@ bool UsedInCol(int grid[N][N], int col, int num)
 }
  
 // Returns whether any assigned entry within the specified 3x3 box matches given number
-bool UsedInBox(int grid[N][N], int boxStartRow, int boxStartCol, int num)
-{
+bool UsedInBox(int grid[N][N], int boxStartRow, int boxStartCol, int num) {
     for (int row = 0; row < 3; row++)
         for (int col = 0; col < 3; col++)
             if (grid[row + boxStartRow][col + boxStartCol] == num)
@@ -109,15 +119,13 @@ bool UsedInBox(int grid[N][N], int boxStartRow, int boxStartCol, int num)
 }
 
 // Returns if it is legal to assign num to given column and row lcoations
-bool isSafe(int grid[N][N], int row, int col, int num)
-{
+bool isSafe(int grid[N][N], int row, int col, int num) {
     return !UsedInRow(grid, row, num) && !UsedInCol(grid, col, num) &&
            !UsedInBox(grid, row - row % 3 , col - col % 3, num);
 }
  
 // Print Grid
-void printGrid(int grid[N][N])
-{
+void printGrid(int grid[N][N]) {
     for (int row = 0; row < N; row++)
     {
         for (int col = 0; col < N; col++)
